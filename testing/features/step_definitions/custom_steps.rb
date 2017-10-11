dc166a0de4700b70570a80dc6d1b12cfca4e9865
James Reidy <jreidy@cornell.edu>
2017-10-11 11:47:14 -0400

    remove unused

--- testing/features/step_definitions/custom_steps.rb
+++ testing/features/step_definitions/custom_steps.rb
@@ -84,9 +84,6 @@ Then("I select the first option from the ares popup") do
   $driver.find_element(:css, path).click
 end
 
-Then("the title should show {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
 
 Then("the first ares reserve title should be {string}") do |string|
   wait.until {
